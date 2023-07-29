cc4650024cba7292318dba1503b9f74b8c7c2da6
Christabel Ajaero <ajaero.christabel@gmail.com>
2023-07-29 14:42:11 +0100

    added two more functions to this app

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,11 @@
 def add(x, y):
     addition = x + y 
     return addition
+
+def subtract(x, y):
+    subtract = x - y 
+    return subtract
+
+def multiply(x, y):
+    multiply = x + y 
+    return multiply
